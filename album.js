05cabf6391af4643e584fe9b355506af2921e98a
Simcha Schaum <simcha.schaum@gmail.com>
2022-01-23 11:05:42 +0200

    added JS functionality to enlarge each album picture and block the background on click

--- album.js
+++ album.js
@@ -1,9 +1,18 @@
-// const imagesArr = document.querySelectorAll(".cd-insert-img");
+const imagesArr = document.querySelectorAll(".cd-insert-img");
+const cover = document.querySelector('#cover');
 
-// imagesArr.forEach((pic,idx)=>{
-//     pic.addEventListener("click", ()=>{
-//         pic.classList.add("enlarged");
-//         console.log(imagesArr)
-//     })
-// })
+imagesArr.forEach((pic,idx)=>{
+    pic.addEventListener("click", ()=>enlarge(idx))
+})
+
+const enlarge = (idx) => {
+    if(!imagesArr[idx].classList.contains('enlarged')){
+        imagesArr[idx].classList.add('enlarged');
+        cover.style.display = "block"
+    } else {
+        imagesArr[idx].classList.remove('enlarged');
+        cover.style.display = "none"
+    }
+
+}
 
